e2df3ff194222df4e4fcfb04a710ae2d497e9520
Sanchit Ingale <89060338+sanchit339@users.noreply.github.com>
2023-02-24 13:19:11 +0530

    minute changes

--- src/com/sanchit/abstractDemo/Parent.java
+++ src/com/sanchit/abstractDemo/Parent.java
@@ -1,7 +1,7 @@
 package com.sanchit.abstractDemo;
 
-
 // if you know the function of the main class needs to be overridden make it abstract
+
 public abstract class Parent {
     // cannot create class of abstract class
     abstract void career(String name);
