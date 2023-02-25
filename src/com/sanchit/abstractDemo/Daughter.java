7710804a92f5d34a1b45dc5582326be6455c5009
Sanchit Ingale <89060338+sanchit339@users.noreply.github.com>
2023-02-25 17:31:21 +0530

    changes

--- src/com/sanchit/abstractDemo/Daughter.java
+++ src/com/sanchit/abstractDemo/Daughter.java
@@ -2,6 +2,7 @@ package com.sanchit.abstractDemo;
 
 public class Daughter extends Parent{
 
+    // extends parent
     @Override
     void career(String name) {
         System.out.println("i want to be" + name);
